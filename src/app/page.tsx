0e41767e7432d5b435aabdebe9cf34e1e3231c13
Johannes Maussner <maussnj@basfad.basf.net>
2025-04-28 08:10:27 +0200

    adds controls to change simulation parameters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,96 @@
 "use client";
 
+import LineChart from "@/components/line-chart";
 import PhasePortrait from "@/components/phase-portrait";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Slider } from "@/components/ui/slider";
 import { reactorModel } from "@/core/reactor-model";
 import { ParentSize } from "@visx/responsive";
+import { useState } from "react";
 
 export default function Home() {
-  const model = reactorModel();
+  const [initial, setInitial] = useState([1.2, 0]);
+  const [parameters, setParameters] = useState<Record<string, number>>({
+    bodenstein: 1,
+    stanton: 1,
+    tau: 400,
+  });
+
+  const handleChange = (value: number[], name: string) => {
+    setParameters({ ...parameters, [name]: value[0] });
+  };
+
+  const model = reactorModel(
+    parameters.stanton,
+    parameters.bodenstein,
+    parameters.tau
+  );
   return (
-    <main className="w-screen h-screen bg-black p-6">
+    <main className="w-screen h-screen p-6">
+      <div className="absolute top-14 left-24 flex flex-col gap-2">
+        <Card className="w-[350px] h-fit ">
+          <CardHeader>
+            <CardTitle>Simulation Parameters</CardTitle>
+            <CardDescription>
+              Modify the below parameters to observe the effect on reactor
+              stability.
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="bodenstein">Boldenstein Number</Label>
+              <Slider
+                id="bodenstein"
+                min={0}
+                max={2}
+                step={0.01}
+                onValueChange={(v) => handleChange(v, "bodenstein")}
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="stanton">Stanton Number</Label>
+              <Slider
+                id="stanton"
+                min={0}
+                max={2}
+                step={0.01}
+                onValueChange={(v) => handleChange(v, "stanton")}
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="tau">Residence Time</Label>
+              <Slider
+                id="tau"
+                min={300}
+                max={1000}
+                step={0.01}
+                onValueChange={(v) => handleChange(v, "tau")}
+              />
+            </div>
+          </CardContent>
+        </Card>
+        <LineChart width={350} height={200} model={model} initial={initial} />
+      </div>
+
       <ParentSize>
         {({ width, height }) => (
-          <PhasePortrait width={width} height={height} model={model} />
+          <PhasePortrait
+            width={width}
+            height={height}
+            model={model}
+            initial={initial}
+            setInitial={setInitial}
+          />
         )}
       </ParentSize>
     </main>
---
+++ src/components/line-chart.tsx
@@ -0,0 +1,100 @@
+import { Card } from "@/components/ui/card";
+import { rungeKuttaIntegration } from "@/core/runge-kutta-integration";
+import { AxisBottom, AxisLeft } from "@visx/axis";
+import { Group } from "@visx/group";
+import { scaleLinear } from "@visx/scale";
+import { LinePath } from "@visx/shape";
+
+const MARGIN = { top: 35, left: 35, right: 35, bottom: 35 };
+
+const LineChart = (props: {
+  width: number;
+  height: number;
+  initial: number[];
+  model: (x: number[]) => number[];
+}) => {
+  const { width, height, model, initial } = props;
+  const innerWidth = width - MARGIN.left - MARGIN.right;
+  const innerHeight = height - MARGIN.top - MARGIN.bottom;
+
+  const timeScale = scaleLinear({
+    range: [0, innerWidth],
+    domain: [0, 15],
+  });
+
+  const yScale = scaleLinear({
+    range: [innerHeight, 0],
+    domain: [0, 2],
+  });
+
+  const x = rungeKuttaIntegration(initial, [0, 15], model);
+
+  const lines = (
+    <Group>
+      <LinePath
+        data={x}
+        y={(d) => yScale(d.states[0])}
+        x={(d) => timeScale(d.time)}
+        stroke={"green"}
+        strokeWidth={2}
+      />
+      <LinePath
+        data={x}
+        y={(d) => yScale(d.states[1])}
+        x={(d) => timeScale(d.time)}
+        stroke={"red"}
+        strokeWidth={2}
+      />
+    </Group>
+  );
+
+  return (
+    <Card className="relative">
+      <svg width={width} height={height}>
+        <Group left={MARGIN.left} top={MARGIN.top}>
+          <AxisBottom
+            scale={timeScale}
+            top={innerHeight}
+            labelProps={{
+              fill: "#98A7C0",
+              textAnchor: "middle",
+              verticalAnchor: "middle",
+              fontSize: 16,
+              fontWeight: 600,
+            }}
+            stroke={"#98A7C0"}
+            tickStroke={"#2A2A2A"}
+            tickLabelProps={{
+              fill: "#98A7C0",
+              textAnchor: "middle",
+              verticalAnchor: "middle",
+              fontSize: 10,
+            }}
+          />
+          <AxisLeft
+            scale={yScale}
+            hideZero
+            labelProps={{
+              fill: "#98A7C0",
+              textAnchor: "middle",
+              verticalAnchor: "middle",
+              fontSize: 16,
+              fontWeight: 600,
+            }}
+            stroke={"#98A7C0"}
+            tickStroke={"#2A2A2A"}
+            tickLabelProps={{
+              fill: "#98A7C0",
+              textAnchor: "end",
+              verticalAnchor: "end",
+              fontSize: 10,
+            }}
+          />
+          {lines}
+        </Group>
+      </svg>
+    </Card>
+  );
+};
+
+export default LineChart;
